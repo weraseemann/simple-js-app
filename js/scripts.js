8f946f32882d1619c51f84a3dd5a4496e8469024
weraseemann <wera.seemann@gmail.com>
2023-12-13 22:52:43 +0100

    Created a new variable called pokemonList
    Added: several objects to the array. Each object represents one Pokémon and has the same keys to avoid errors. Added at least three keys to each Pokémon object and made up a value for each of them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favouriteFood='Sushi';
-document.write(favouriteFood);
+let pokemonList = [
+    {
+    name1: 'Bulbasaur',
+    height: 7, 
+    type:['grass', 'poison']
+    },
+    {
+    name2: 'Venusaur',
+    height: 2, 
+    type:['grass', 'poison']
+    },
+    {
+    name3: 'Charmander',
+    height: 0.6, 
+    type:'fire'
+    }
+]
