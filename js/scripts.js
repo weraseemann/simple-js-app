30f383edf668c15ab7b0182168571262ec7dd4ad
weraseemann <wera.seemann@gmail.com>
2023-12-21 23:10:03 +0100

    for Each vs. for to print property details
    Used a forEach() function instead of the for loop in order to print the details of each pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,19 @@ let pokemonList = [
     type:'fire'
     }
 ]
-for (let i=0; i<pokemonList.length; i++){
-    if (pokemonList[i].height>3)
-    {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"+ " - WOW! That's big!<br>");
-} else 
-    {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
-}
-}
+// for (let i=0; i<pokemonList.length; i++){
+//     if (pokemonList[i].height>3)
+//     {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"+ " - WOW! That's big!<br>");
+// } else 
+//     {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+// }
+// }
+
+// Used a forEach() function instead of the for loop.
+pokemonList.forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name +' (height: ' + pokemon.height + '; type: ' + pokemon.type + ')</p>');
+});
+//Another way: 
+// function myLoopFunction(pokemon) {document.write('<p>' + pokemon.name +' (height: ' + pokemon.height + '; type: ' + pokemon.type + ')</p>');
+// };
+// pokemonList.forEach(myLoopFunction);
