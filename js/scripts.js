35eea6228575c1d5b8b6a842a7c5b301e927f843
weraseemann <wera.seemann@gmail.com>
2023-12-22 01:00:53 +0100

    Added IIFE for pokemonRepository array. Adjusted  forEach loop
    Created a new pokemonRepository variable to hold what IIFE will return, then assigned the IIFE to that variable.
    The IIFE returns an object with the following public functions assigned as keys: getAll and add.
    Adjusted the forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,37 @@ pokemonList.forEach(function(pokemon) {
 // function myLoopFunction(pokemon) {document.write('<p>' + pokemon.name +' (height: ' + pokemon.height + '; type: ' + pokemon.type + ')</p>');
 // };
 // pokemonList.forEach(myLoopFunction);
+
+let pokemonRepository = (function() {
+    let pokemonList = [
+      {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+      {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+      {name: 'Charmander', height: 0.6, type: 'fire'}
+    ];
+    
+      function add(pokemon) {
+        pokemonList.push(pokemon);
+      }
+      function getAll() {
+        return pokemonList;
+      }
+      
+    return {
+      
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add({name: 'Charmeleon', height: 3.07, type: 'fire'});
+  console.log(pokemonRepository.getAll());
+
+//to print the repository 
+pokemonRepository.getAll().forEach(function(pokemon) {
+     if (pokemon.height > 3) {
+         document.write('<p>', pokemon.name + ' - ' + pokemon.height + ' Wow, that\'s big!; </p>');
+    } else {
+        document.write('<p>', pokemon.name + ' - ' + pokemon.height + '; </p>');
+    }
+});
