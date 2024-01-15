1d7b73f27920e23332a226c32817ab7a6f592549
weraseemann <wera.seemann@gmail.com>
2024-01-15 15:05:22 +0100

    Added modal for Pokemonlist, cleaned  code
    Added modal for Pokemonlist with name and hight, cleaned unnecesarry code

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,18 @@ let pokemonRepository = (function() {
 
     
       function add(pokemon) {
+        if (typeof (pokemon) === 'object' && 'name' in pokemon && 'detailsUrl'in pokemon) {
         pokemonList.push(pokemon);
-      }
+         } else {
+        console.log('wrong data type');
+         }
+        }
     
-
       function getAll() {
         return pokemonList;
+
       }
+      //add unordered list <ul>, list of items <li> and <button> with innerText
       function addListItem(pokemon) {
         let pokemonList = document.querySelector('.pokemon-list');
         let listpokemon = document.createElement('li');
@@ -25,13 +30,13 @@ let pokemonRepository = (function() {
       }
     
       function loadList() {
-        return fetch (apiUrl).then(function (response){
+        return fetch (apiUrl).then(function (response) {
             return response.json();
         }).then(function (json){
             json.results.forEach(function(item){
                 let pokemon = {
                     name: item.name,
-                    detailsUrl: item.url
+                    detailsUrl: item.url,
                 };
                 add(pokemon);
             });
@@ -44,18 +49,77 @@ let pokemonRepository = (function() {
         return fetch(url).then(function (response){
             return response.json();
         }).then(function (details){
-            item.imageUrl = details.sprites.front_default;
+            item.imgUrl = details.sprites.front_default;
             item.height = details.height;
-        }).catch(function(e){
+            item.types =details.types;
+        }).catch(function (e){
             console.error(e);
         });
       }
+    
       function showDetails(item) {
-        pokemonRepository.loadDetails(item).then(function(){
-            console.log(item);
-        });
+        pokemonRepository.loadDetails(item).then(function() {
+            showModal(item.name, 'Height: ' + item.height);
+      });
+      }   
+
+      let modalContainer = document.querySelector('#modal-container');
+        
+      function showModal(title, text) {
+        modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let closeButtonElement = document.createElement('button'); //add Close-button
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p'); 
+        contentElement.innerText = text;
+       
+
+        // let imgElement = document.createElement('img');
+        // imgElement.src = item.imgUrl; 
+        // imgElement.alt = item.name;
+
+        modal.appendChild(closeButtonElement); 
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        // modal.appendChild(imgElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
       }
 
+      function hideModal() { //to remove the modal by clicking to ESC
+        modalContainer.classList.remove('is-visible');
+      }
+    
+        window.addEventListener('keydown', (e) => { // to remove the modal with ESC key
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+        });
+
+    //to remove the modal when user cklicks outside of the modal
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay (parentElement)
+        modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+                hideModal();
+        }
+        });
+
+        // document.querySelector('#show-modal').addEventListener('click', () => {
+        //     showModal('Modal title', 'Modal text!');
+        //     });
+        
     return {
       
       add: add,
@@ -63,25 +127,12 @@ let pokemonRepository = (function() {
       addListItem: addListItem,
       loadList: loadList,
       loadDetails: loadDetails,
-      showDetails:showDetails,
+      showDetails: showDetails,
+      showModal: showModal,
     };
   })();
 
-
-  console.log(pokemonRepository.getAll());
-
-//To add a pokemon to a repository:
-//pokemonRepository.add({name: 'Charmeleon', height: 3.07, type: 'fire'});
-//   console.log(pokemonRepository.getAll());
-
-//to print the repository 
-// pokemonRepository.getAll().forEach(function(pokemon) {
-//      if (pokemon.height > 3) {
-//          document.write('<p>', pokemon.name + ' - ' + pokemon.height + ' Wow, that\'s big!; </p>');
-//     } else {
-//         document.write('<p>', pokemon.name + ' - ' + pokemon.height + '; </p>');
-//     }
-// });
+console.log(pokemonRepository.getAll());
 
 pokemonRepository.loadList().then(function(){
     pokemonRepository.getAll().forEach(function (pokemon) {
@@ -89,95 +140,4 @@ pokemonRepository.loadList().then(function(){
 });
 });
 
-// let pokemonList = [
-//     {
-//     name: 'Bulbasaur',
-//     height: 7, 
-//     type:['grass', 'poison']
-//     },
-//     {
-//     name: 'Venusaur',
-//     height: 2, 
-//     type:['grass', 'poison']
-//     },
-//     {
-//     name: 'Charmander',
-//     height: 0.6, 
-//     type:'fire'
-//     }
-// ]
-// for (let i=0; i<pokemonList.length; i++){
-//     if (pokemonList[i].height>3)
-//     {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"+ " - WOW! That's big!<br>");
-// } else 
-//     {document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
-// }
-// }
 
-// Used a forEach() function instead of the for loop.
-// pokemonList.forEach(function (pokemon) {
-//     document.write('<p>' + pokemon.name +' (height: ' + pokemon.height + '; type: ' + pokemon.type + ')</p>');
-// });
-//Another way: 
-// function myLoopFunction(pokemon) {document.write('<p>' + pokemon.name +' (height: ' + pokemon.height + '; type: ' + pokemon.type + ')</p>');
-// };
-// pokemonList.forEach(myLoopFunction);
-
-// let pokemonRepository = (function() {
-//     let repository = [
-//       {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-//       {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
-//       {name: 'Charmander', height: 0.6, type: 'fire'},
-//     ];
-    
-//       function add(pokemon) {
-//         repository.push(pokemon);
-//       }
-//       function getAll() {
-//         return repository;
-//       }
-//       function addListItem(pokemon) {
-//         let pokemonList = document.querySelector('.pokemon-list');
-//         let listpokemon = document.createElement('li');
-//         let button = document.createElement('button');
-//         button.innerText = pokemon.name;
-//         button.classList.add('button-class');
-//         listpokemon.appendChild(button);
-//         pokemonList.appendChild(listpokemon);
-//         button.addEventListener('click', function (event) {
-//             console.log(event);
-//           });
-//         button.addEventListener('click', function (event) {
-//             showDetails(pokemon);
-//         });
-//       }
-//       function showDetails(pokemon) {
-//           console.log(pokemon);
-//       }
-//     return {
-      
-//       add: add,
-//       getAll: getAll,
-//       addListItem: addListItem,
-//       showDetails:showDetails,
-//     };
-//   })();
-
-
-//   console.log(pokemonRepository.getAll());
-//   pokemonRepository.add({name: 'Charmeleon', height: 3.07, type: 'fire'});
-//   console.log(pokemonRepository.getAll());
-
-// //to print the repository 
-// // pokemonRepository.getAll().forEach(function(pokemon) {
-// //      if (pokemon.height > 3) {
-// //          document.write('<p>', pokemon.name + ' - ' + pokemon.height + ' Wow, that\'s big!; </p>');
-// //     } else {
-// //         document.write('<p>', pokemon.name + ' - ' + pokemon.height + '; </p>');
-// //     }
-// // });
-
-// pokemonRepository.getAll().forEach(function (pokemon) {
-//     pokemonRepository.addListItem(pokemon);
-
-// });
